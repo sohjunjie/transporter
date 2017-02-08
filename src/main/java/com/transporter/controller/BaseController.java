f93216883b3691f491eecd5666788ddd56ce7f74
sohjunjie <junjie.soh93@gmail.com>
2017-02-08 16:11:09 +0800

    clean up unused models and files

--- src/main/java/com/transporter/controller/BaseController.java
+++ src/main/java/com/transporter/controller/BaseController.java
@@ -13,17 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.transporter.model.Student;
 import com.transporter.model.user.AuthenticatedUser;
 import com.transporter.model.user.LTAPersonnel;
 import com.transporter.service.AuthenticatedUserService;
-import com.transporter.service.StudentService;
 
 @Controller
 public class BaseController {
 
-	@Autowired
-	private StudentService studentService;
 	@Autowired
 	private AuthenticatedUserService authUserService;
 
@@ -56,46 +52,6 @@ public class BaseController {
 		session.invalidate();
 		return "redirect:/";
 
-	}
-	
-	@RequestMapping("/index")
-	public String setupForm(Map<String, Object> map, HttpSession session){
-		Student student = new Student();
-		map.put("student", student);
-		map.put("studentList", studentService.getAllStudent());
-		
-		System.out.println(session.getAttribute("firstname"));
-		
-		return "student";
-	}
-	
-	@RequestMapping(value="/student.do", method=RequestMethod.POST)
-	public String doActions(@ModelAttribute Student student, BindingResult result, @RequestParam String action, Map<String, Object> map, HttpSession session){
-		
-		session.setAttribute("firstname", student.getFirstname());
-		
-		Student studentResult = new Student();
-		switch(action.toLowerCase()){	//only in Java7 you can put String in switch
-		case "add":
-			studentService.add(student);
-			studentResult = student;
-			break;
-		case "edit":
-			studentService.edit(student);
-			studentResult = student;
-			break;
-		case "delete":
-			studentService.delete(student.getStudentId());
-			studentResult = new Student();
-			break;
-		case "search":
-			Student searchedStudent = studentService.getStudent(student.getStudentId());
-			studentResult = searchedStudent!=null ? searchedStudent : new Student();
-			break;
-		}
-		map.put("student", studentResult);
-		map.put("studentList", studentService.getAllStudent());
-		return "student";
 	}
 	
 }
