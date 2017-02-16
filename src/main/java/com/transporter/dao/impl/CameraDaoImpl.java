f452c33af7922209373a9e81c40ce1df9c06a6ea
sohjunjie <junjie.soh93@gmail.com>
2017-02-16 21:36:01 +0800

    add anotations to camera

--- src/main/java/com/transporter/dao/impl/CameraDaoImpl.java
+++ src/main/java/com/transporter/dao/impl/CameraDaoImpl.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.transporter.dao.CameraDao;
-import com.transporter.model.AccidentReport;
 import com.transporter.model.Camera;
 import com.transporter.model.Camera.CameraStatus;
 import com.transporter.model.Camera.CameraType;
 
+@Repository
 public class CameraDaoImpl implements CameraDao {
 
 	@Autowired
