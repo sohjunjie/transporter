fbfa305bb9eda61f57927b75971e6a3dcedb054a
Woof Woof <Woof Woof@10.27.148.134>
2017-03-14 11:33:30 +0800

    add comments

--- src/main/java/com/transporter/model/AccidentCause.java
+++ src/main/java/com/transporter/model/AccidentCause.java
@@ -17,16 +17,19 @@ public class AccidentCause {
 	private int causeId;
 	@Column
 	private String cause;
-
+//method to get the ID number of the cause
 	public int getCauseId() {
 		return causeId;
 	}
+//method to set the ID number for the cause
 	public void setCauseId(int causeId) {
 		this.causeId = causeId;
 	}
+//method to get the cause of accident
 	public String getCause() {
 		return cause;
 	}
+//method to set the cause of accident
 	public void setCause(String cause) {
 		this.cause = cause;
 	}	
--- src/main/java/com/transporter/model/AccidentReport.java
+++ src/main/java/com/transporter/model/AccidentReport.java
@@ -51,91 +51,117 @@ public class AccidentReport {
 	@ManyToOne(cascade=CascadeType.PERSIST)
 	@JoinColumn(name="resolvedBy")
 	private LTAPersonnel resolvedBy;
-
+//check if a pending report is approced ro resolved
 	public boolean isPending(){
 		return !(isApproved() || isResolved());
 	}
+//check if a report is approved
 	public boolean isApproved(){
 		return (this.approvedBy != null);
 	}
+//check if a report is resolved 
 	public boolean isResolved(){
 		return (this.resolvedBy != null);
 	}
-	
-	public AccidentReport(){ }
 
+	public AccidentReport(){ }
+//method to get the ID number of the accident report
 	public int getReportId() {
 		return reportId;
 	}
+//method to set the ID number of the accident report
 	public void setReportId(int reportId) {
 		this.reportId = reportId;
 	}
+//method to get the location of the accident 
 	public String getFormattedAddress() {
 		return formattedAddress;
 	}
+//method to set the location of the accident
 	public void setFormattedAddress(String formattedAddress) {
 		this.formattedAddress = formattedAddress;
 	}
+//method to get the time of the accident 
 	public Date getAccidentDateTime() {
 		return accidentDateTime;
 	}
+//method to set the time of the accident 
 	public void setAccidentDateTime(Date reportedDateTime) {
 		this.accidentDateTime = reportedDateTime;
 	}
+//method to get the number of casualties in the accident 
 	public int getNumOfCasualties() {
 		return numOfCasualties;
 	}
+//method to set the number of casualties in the accident 
 	public void setNumOfCasualties(int numOfCasualties) {
 		this.numOfCasualties = numOfCasualties;
 	}
+//method to get the description of the accident 
 	public String getDescription() {
 		return this.description;
 	}
+//method to set the description of the accident
 	public void setDescription(String description) {
 		this.description = description;
 	}
+//method to get the longitude coordinate of the accident's location
 	public double getLongitude() {
 		return longitude;
 	}
+//method to set the longitude coordinate of the accident's location
 	public void setLongitude(double longitude) {
 		this.longitude = longitude;
 	}
+//method to get the latitude coordinate of the accident's location
 	public double getLatitude() {
 		return latitude;
 	}
+//method to set the longitude coordinate of the accident's location
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
+//method to get the link of the image of accident
 	public String getImageLink() {
 		return imageLink;
 	}
+//method to set the link of the image of accident
 	public void setImageLink(String imageLink) {
 		this.imageLink = imageLink;
 	}
+//method to get the date that the accident is approved
 	public Date getApprovedDateTime() {
 		return approvedDateTime;
 	}
+//method to set the date that the accident is approved
 	public void setApprovedDateTime(Date approvedDateTime) {
 		this.approvedDateTime = approvedDateTime;
 	}
+//method to get the details of the LTA personnel who approves the accident report
 	public LTAPersonnel getApprovedBy() {
 		return approvedBy;
 	}
+//method to set the details of the LTA personnel who approves the accident report 
 	public void setApprovedBy(LTAPersonnel approvedBy) {
 		this.approvedBy = approvedBy;
 	}
+//method to get the date that the accident is resolved
 	public Date getResolvedDateTime() {
 		return resolvedDateTime;
 	}
+//method to set the date that the accident is resolved
 	public void setResolvedDateTime(Date resolvedDateTime) {
 		this.resolvedDateTime = resolvedDateTime;
 	}
+//method to get the official cause of accident
 	public AccidentCause getOfficialCause() {
 		return officialCause;
 	}
+//method to set the official cause of accident
 	public void setOfficialCause(AccidentCause officialCause) {
 		this.officialCause = officialCause;
 	}
+//method to get the details of the LTA personnel who resolves the 
 	public LTAPersonnel getResolvedBy() {
 		return resolvedBy;
 	}
